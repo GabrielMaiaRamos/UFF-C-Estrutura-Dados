217f9979e7a921d5576dea48854e078d2f24293e
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-04-08 00:10:10 -0300

    avaliacao1_resolvida
    identificar no zigzague

--- avaliacoes.C
+++ avaliacoes.C
@@ -9,34 +9,21 @@ typedef struct sNoA
     struct sNoA *dir;
 } TNoA;
 
-int zigAtual(TNoA *no, int dir)
-{
-    if (no == NULL)
-        return 1;
-    if (dir == -1) // veio da esquerda (precisa ir pra direita)
-    {
-        if (no->dir == NULL)
-            return 0;
-        else
-            return zigAtual(no->dir, 1);
-    }
-    else if (dir == 1) // veio da direita (precisa ir pra esquerda)
-        if (no->esq == NULL)
-            return 0;
-        else
-            return zigAtual(no->esq, -1);
-}
-
 int ehZigueZague(TNoA *raiz)
 {
     if (raiz == NULL)
-        return 0;
-    if (raiz->esq != NULL && raiz->dir == NULL)
-        return zigAtual(raiz->esq, -1);
-    else if (raiz->esq == NULL && raiz->dir != NULL)
-        return zigAtual(raiz->dir, 1);
-    else
-        return 0;
+        return 1;
+    while (raiz != NULL)
+    {
+        if (raiz->dir != NULL && raiz->esq != NULL)
+            return 0;
+        else if (raiz->dir != NULL)
+            raiz = raiz->dir;
+        else if (raiz->esq != NULL)
+            raiz = raiz->esq;
+        else
+            return 1;
+    }
     return 0;
 }
 
