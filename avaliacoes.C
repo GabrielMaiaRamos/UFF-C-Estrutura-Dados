697295122424f99118a03b07ea65ec83461e6837
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-04-21 11:57:02 -0300

    avaliacao_feita

--- avaliacoes.C
+++ avaliacoes.C
@@ -9,6 +9,21 @@ typedef struct sNoA
     struct sNoA *dir;
 } TNoA;
 
+typedef struct vizinho
+{
+    int id_vizinho;
+    int peso;
+    struct vizinho *prox;
+} TVizinho;
+
+typedef struct grafo
+{
+    int id_vertice;
+    int cor;
+    TVizinho *prim_vizinho;
+    struct grafo *prox;
+} TGrafo;
+
 int ehZigueZague(TNoA *raiz)
 {
     if (raiz == NULL)
@@ -27,7 +42,28 @@ int ehZigueZague(TNoA *raiz)
     return 0;
 }
 
+int ehNaoOrientado(TGrafo *g)
+{
+    TGrafo *v1 = g, *v2 = NULL;
+    while (v1 != NULL)
+    {
+        TVizinho *v1Viz = v1->prim_vizinho;
+        while (v1Viz != NULL) // enquanto houverem arestas, roda
+        {
+            v2 = busca_vertice(g, v1Viz->id_vizinho); // encontra o vertice original desse vizinho
+            TVizinho *v2Viz = busca_vizinho(v2->prim_vizinho, v1->id_vertice);
 
+            if (v2Viz == NULL) // se NEM encontrar a aresta, nao eh ordenado
+                return 0;
+            if (v1Viz->peso != v2Viz->peso) // se os pesos forem diferentes, nao eh ordenado
+                return 0;
+            // se nao forem os casos, esta correto e passa pro proximo
+            v1Viz = v1Viz->prox;
+        }
+        v1 = v1->prox;
+    }
+    return 1;
+}
 
 int main()
 {
