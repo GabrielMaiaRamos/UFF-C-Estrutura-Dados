fea575e43cd12cf3339cc309d403d765da00b4f8
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-04-30 01:15:07 -0300

    ex4 feito

--- lista_ab_busca.c
+++ lista_ab_busca.c
@@ -89,3 +89,49 @@ TABB *retira_impares(TABB *a)
     else
         return a; // se for par, retorna o no sem excluir
 }
+
+/*4. Escreva uma função em C que, dada uma árvore binária de busca qualquer, retorne, num vetor, todos os
+elementos menores que N. A função deve ter o seguinte protótipo: int* mN(TABB*a, int N); */
+int tamanho_do_vetor(TABB *a, int N)
+{
+    if (a == NULL)
+        return 0;
+
+    if (a->info >= N) // se for maior, vai somente pra esquerda
+        return tamanho_do_vetor(a->esq, N);
+
+    // se for menor, conta todo mundo a partir dali
+    return 1 + tamanho_do_vetor(a->dir, N) + tamanho_do_vetor(a->esq, N);
+}
+
+void preenche_vetor(TABB *a, int N, int *vet, int *pos)
+{
+    if (a == NULL)
+        return;
+    if (a->info >= N)
+    {
+        preenche_vetor(a->esq, N, vet, pos);
+        return;
+    }
+    // daqui pra baixo, "a" sempre sera menor que N
+
+    vet[*pos] = a->info; // coloca o valor no vetor
+    (*pos)++;            // anda a posicao
+
+    preenche_vetor(a->esq, N, vet, pos); // chama pra esquerda e direita
+    preenche_vetor(a->dir, N, vet, pos);
+}
+
+int *mN(TABB *a, int N)
+{
+    int tam = tamanho_do_vetor(a, N);
+    if (tam = 0)
+        return NULL;
+
+    int *vet = malloc(sizeof(int) * tam);
+    int pos = 0;
+
+    preenche_vetor(a, N, vet, &pos);
+
+    return vet;
+}
