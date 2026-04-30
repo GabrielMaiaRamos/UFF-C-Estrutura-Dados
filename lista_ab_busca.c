bb1d856b2f466096c990497f5d22a727e22d19aa
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-04-29 22:52:51 -0300

    lista_binaria_de_busca ex1 feito

---
+++ lista_ab_busca.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct ab
+{
+    int info;
+    struct ab *esq, *dir;
+} TABB;
+
+/*[1] Escreva uma função em C para encontrar o maior elemento da árvore: TABB* maior(TAB *a);*/
+TABB *maior(TABB *a)
+{
+    if (a == NULL)
+        return NULL;
+
+    TABB *aux = a;
+    while (aux->dir != NULL)
+        aux = aux->dir;
+    return aux;
+}
+
+/*[2] Escreva uma função em C para encontrar o menor elemento da árvore: TABB* menor(TAB *a);*/
+TABB *menor(TABB *a)
+{
+}
