90c1e8cb5230b6668ce1b1d9a4d9938dade3d030
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-04-29 22:55:38 -0300

    ex2 feito

--- lista_ab_busca.c
+++ lista_ab_busca.c
@@ -22,4 +22,11 @@ TABB *maior(TABB *a)
 /*[2] Escreva uma função em C para encontrar o menor elemento da árvore: TABB* menor(TAB *a);*/
 TABB *menor(TABB *a)
 {
+    if (a == NULL)
+        return NULL;
+
+    TABB *aux = a;
+    while (aux->esq != NULL)
+        aux = aux->esq;
+    return aux;
 }
