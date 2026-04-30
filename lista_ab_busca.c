cbcbc846b93c473f434f896f7b8ff5f0ca31361b
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-04-30 00:24:50 -0300

    ex3 feito

--- lista_ab_busca.c
+++ lista_ab_busca.c
@@ -30,3 +30,62 @@ TABB *menor(TABB *a)
         aux = aux->esq;
     return aux;
 }
+
+/*3. Escreva uma função em C que, dada uma árvore binária de busca qualquer, retire todos os elementos ímpares
+da árvore original. A função deve ter o seguinte protótipo: TABB* retira_impares(TABB* a);*/
+
+TABB *retira_maior(TABB *a, int *valor_substituido) // auxiliar pra retirar o no maior da esquerda e ja guardar o valor dele
+{
+    if (a->dir == NULL)
+    {
+        *valor_substituido = a->info; // pega o valor substituido
+        TABB *temp = a->esq;
+        free(a);
+        return temp;
+    }
+    a->dir = retira_maior(a->dir, valor_substituido); // passa pra direita caso ainda tenha no na direita
+    return a;
+}
+
+TABB *retira_impares(TABB *a)
+{
+    if (a == NULL)
+        return NULL;
+
+    retira_impares(a->esq);
+    retira_impares(a->dir);
+
+    if (a->info % 2 != 0)
+    {
+        // CASO A: NO FOLHA
+        if (a->dir == NULL && a->esq == NULL)
+        {
+            free(a);
+            return NULL;
+        }
+        // CASO B1: NO COM UM FILHO NA DIREITA
+        else if (a->esq == NULL)
+        {
+            TABB *temp = a->dir;
+            free(a);
+            return temp;
+        }
+        // CASO B2: NO COM UM FILHO NA ESQUERDA
+        else if (a->dir == NULL)
+        {
+            TABB *temp = a->esq;
+            free(a);
+            return temp;
+        }
+        // CASO C: NO COM DOIS FILHOS
+        else
+        {
+            int valor_substituido;
+            // retira o maior no da arvore da esquerda e tras o valor dele
+            a->esq = retira_maior(a->esq, &valor_substituido);
+            a->info = valor_substituido;
+        }
+    }
+    else
+        return a; // se for par, retorna o no sem excluir
+}
